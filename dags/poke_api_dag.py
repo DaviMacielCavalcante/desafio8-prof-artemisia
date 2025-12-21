c6700f4b72a42637aca4b5b6eb0fed9360cb9503
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2025-12-20 21:01:12 -0300

    ✨feat: geração da camada silver

--- dags/poke_api_dag.py
+++ dags/poke_api_dag.py
@@ -218,9 +218,131 @@ def pokemon_etl_pipeline():
             "key": final_table_key
         }
     
-    with TaskGroup(group_id="extract") as extract_group:
+    @task(task_id="generate_silver_table_to_minio")
+    def generate_silver_table_to_minio(bronze_info: dict):
+        import pyarrow as pa
+        import pyarrow.parquet as pq
+        import boto3
+        from airflow.sdk.definitions.variable import Variable
+        from pipe.utils.etl_utils import check_list_of_struct, check_struct, clean_bools, clean_numerics, clean_string, clean_url
+        from datetime import datetime, timezone
+        from io import BytesIO
+
+
+        bucket = bronze_info["bucket"]
+        bronze_key = bronze_info["key"]
+
+        MINIO_USER = Variable.get("MINIO_ROOT_USER")
+        MINIO_PASSWORD = Variable.get("MINIO_ROOT_PASSWORD")
+
+        s3 = boto3.client(
+            "s3",
+            endpoint_url="http://minio:9000",
+            aws_access_key_id=MINIO_USER,
+            aws_secret_access_key=MINIO_PASSWORD
+        )
+
+
+        obj = s3.get_object(
+                Bucket=bucket,
+                Key=bronze_key
+            )
+        
+        buffer = BytesIO(obj["Body"].read())
+
+        pokemons_parquet = pq.ParquetFile(buffer)
+        
+        pokemon_schema = pokemons_parquet.schema_arrow
+        
+        numeric_columns = ["id", "base_experience", "height", "weight", "order"]
+        string_columns = ["name"]
+        bool_columns = ["is_default"]
+        url_columns = ["location_area_encounters"]
+        list_struct_columns = ["abilities", "types", "stats", "moves", 
+            "game_indices", "forms", "held_items",
+            "past_types", "past_abilities"]
+        struct_columns = ["sprites", "cries", "species"]
+
+        processed_columns = {}
+
+        for col in pokemon_schema.names:
+
+            processed_column = None
+    
+            table_col = pokemons_parquet.read(columns=[col])
+            col_as_array = table_col.column(col)
+            
+            if col in numeric_columns:
+                processed_column = clean_numerics(col_as_array)
+
+            if col in string_columns:
+                processed_column = clean_string(col_as_array)
+
+            if col in bool_columns:
+                processed_column = clean_bools(col_as_array)
+
+            if col in url_columns:
+                processed_column = clean_url(col_as_array)
+
+            if col in list_struct_columns:
+                processed_column = check_list_of_struct(col_as_array, col)
+
+            if col in struct_columns:
+                processed_column = check_struct(col_as_array, col)
+
+        
+    
+            processed_columns[col] = processed_column if processed_column else col_as_array
+
+            del table_col, col_as_array
+
+
+        pokemons_all_infos_silver = pa.Table.from_arrays(
+        arrays=[processed_columns[name] for name in pokemon_schema.names],
+        names = pokemon_schema.names
+        )
+
+        metadata = {
+        b"source": bronze_key.encode(),
+        b"datetime_utc": datetime.now(timezone.utc).isoformat().encode(),
+        b"layer": b"silver",
+        b"pipeline": b"pokemon_etl_pipeline",
+        b"compression": b"snappy",
+        b"validations": b"numeric_ranges,text_cleanup,struct_validation"
+        }
+
+        pokemons_all_infos_silver = pokemons_all_infos_silver.replace_schema_metadata(metadata)
+
+        buffer = BytesIO()
+        pq.write_table(pokemons_all_infos_silver, buffer, compression="snappy")
+        buffer.seek(0)
+
+        pokemons_all_infos_silver_key = "silver/pokemons_all_info_silver.parquet"
+
+
+        s3.put_object(
+            Bucket=bucket,
+            Key=pokemons_all_infos_silver_key,
+            Body=buffer.getvalue()
+        )
+
+        del pokemons_all_infos_silver, buffer 
+
+        return {
+            "bucket": bucket,
+            "key": pokemons_all_infos_silver_key
+        }
+
+    
+    with TaskGroup(group_id="bronze") as bronze:
         api_response = fetch_pokemons_ids_data()
         pokemons_infos = fetch_all_pokemon_data(api_response)
-        bucket = generate_bronze_table_to_minio(pokemons_infos)
+        bronze_bucket = generate_bronze_table_to_minio(pokemons_infos)
+
+    with TaskGroup(group_id="silver") as silver:
+        silver_bucket = generate_silver_table_to_minio(bronze_bucket)
+
+    bronze >> silver
+
 
 pokemon_dag = pokemon_etl_pipeline()
--- pipe/utils/etl_utils.py
+++ pipe/utils/etl_utils.py
@@ -1,4 +1,6 @@
 import requests
+import pyarrow as pa
+import pyarrow.compute as pc
 def fetch_all_pokemons(url: str):
 
     try:
@@ -37,3 +39,60 @@ def generate_list_of_all_infos_on_pokemon(pokemon_dict):
     for k in pokemon_entity_bronze.keys():
         pokemon_entity[k] = pokemon_dict[k]
     return pokemon_entity
+
+def clean_numerics(array):
+    
+    col_without_nulls = pc.fill_null(array, 0)
+
+    col_int64 = pc.cast(col_without_nulls, pa.int64())
+
+    negatives = pc.less(col_int64, 0)
+
+    col_no_negatives = pc.if_else(negatives, 0, col_int64)
+
+    return col_no_negatives
+
+def clean_string(array):
+
+    lower_case_strings = pc.utf8_lower(array)
+
+    trimmed_strings = pc.utf8_trim_whitespace(lower_case_strings)
+
+    not_null_strings = pc.fill_null(trimmed_strings, "")
+
+    return not_null_strings
+
+def clean_bools(array):
+
+    boll_without_null = pc.fill_null(array, False)
+
+    col_bool = pc.cast(boll_without_null, pa.bool_())
+
+    return col_bool
+
+def clean_url(array):
+
+    trimmed = pc.utf8_trim_whitespace(array)
+
+    return trimmed
+
+def check_list_of_struct(array, col_name):
+    
+    if col_name == "types":
+        types_lenghts = pc.list_value_length(array)
+
+        nulls = pc.equal(types_lenghts, 0)
+
+    return array
+
+def check_struct(array, col_name):
+
+    if col_name == "species":
+        species_name = pc.struct_field(array, "name")
+
+        null_number = pc.sum(pc.is_null(species_name)).as_py()
+
+        if null_number > 0:
+             print(f"AVISO: {null_number} registros com {species_name} null")
+
+    return array
